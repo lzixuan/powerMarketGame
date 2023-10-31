1e8135de291b08cfa11986b3fbf6176e0d8bc4e9
lzixuan <395862131@qq.com>
2023-10-31 12:51:05 -0500

    Deployment version.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # powerMarketGame
-An interactive game that simulates wholesale power market
+An interactive game that simulates wholesale power market.
+## Setup
+1. `pip3 install -r requirements.txt`.
+2. `python3 mainApp.py 8000` (or other port number).
+3. Go to `localhost:8000` in your browser.
+4. For one-person mode, please input 1000 as game ID and click "clear market" until the game advances to period 4, and then you can click "go to next round" to move to round 2.
+5. If you encounter any errors, you can restart the program and re-enter the game in browser.
--- mainApp.py
+++ mainApp.py
@@ -6,10 +6,15 @@ from functools import partial
 import time
 import asyncio
 import json
+import plotly
 import plotly.express as px
 from gridDispatch import gridDispatch
 import pandas as pd
 import numpy as np
+import sys
+import random
+import os
+import pickle
 
 gameID_role = {
     1: {
@@ -37,9 +42,10 @@ gameID_role = {
 roleID_gameID = {}
 
 roleDescription = [
-    'Plants that are running continuously over time and used to cater the base demand of the grid are said to be base-load power plants. Examples include nuclear, coal-fired, and combined cycles.\nYour power plant has large generation capacity and low marginal cost. But because of some physical and mechanical constraints (e.g. start or change output slowly), you will be penalized when not being dispatched.\nObjective: Maximize profit = market revenue - generation cost - penalty of not being dispatched\nOther Attributes:',
-    'Renewable power plants are pivotal components of the modern and future power grid, providing clean and sustainable sources of electricity. They harness energy from naturally occurring and replenishable resources like sunlight, wind, water, and geothermal heat.\nYou own a wind/solar power plant, whose generation depends on the weather but comes at nearly zero cost. In addition, you can get government tax credit for the generation you sell (assume $50/MWh). In the power market, you want to sell as much as possible of your generation so that you can earn back your investment sooner.\nObjective: maximize market revenue=market revenue + tax credit',
-    'As the gamemaster, you are equivalent to load-serving entities (e.g. ComEd) and grid operator. The "Clear Market" button will trigger a dispatch solver that selects the least-cost combination of generator bids to meet the load in two places and advance the game to next period.'
+    'Plants that are running continuously over time and used to cater the base demand of the grid are said to be base-load power plants. Examples include nuclear, coal-fired, and combined cycles.\nYour power plant has large generation capacity and low marginal cost. But because of some physical and mechanical constraints (e.g. start or change output slowly), you will be penalized when not being dispatched (dispatch result=0).\n\nObjective: Maximize profit = market revenue - generation cost - penalty of not being dispatched\nOther Attributes:',
+    'Renewable power plants are pivotal components of the modern and future power grid, providing clean and sustainable sources of electricity. They harness energy from naturally occurring and replenishable resources like sunlight, wind, water, and geothermal heat.\nYou own a wind/solar power plant, whose generation depends on the weather but comes at nearly zero cost. In addition, you can get government tax credit for the generation you sell (assume $30/MWh). In the power market, you want to sell as much as possible of your generation so that you can earn back your investment sooner.\n\nObjective: maximize market revenue=market revenue + tax credit\nOther Attributes:',
+    'Peaking power plants are a vital component of the power grid, designed to provide electricity during periods of high demand or peak load. Their generation can ramp up fast in times of capacity shortage. Examples include diesel and natural gas-fired power plants.\nNow you own such a generator with high generation cost. Can you identify the time periods when there is a potential gap between demand and low-cost supply as they are your opportunities to earn high revenue?\n\nObjective: Maximize profit=market revenue - generation cost\nOther Attributes:',
+    'As the gamemaster, you cover load-serving entities (e.g. ComEd) and grid operator. The "Clear Market" button will trigger a dispatch solver that selects the least-cost combination of generator bids to meet the load in two places and advance the game to next period.'
 ]
 
 roleByRealPlayer = [False for i in range(0, 6)]
@@ -48,11 +54,11 @@ round = 1
 period = 1
 
 # renewable generation and load profiles
-windProfile = [0.7, 0.4, 0.3, 0.6]
+windProfile = [0.7, 0.3, 0.3, 0.6]
 solarProfile = [0, 0.9, 0.8, 0]
 locIdx_name = {0: 'South', 1: 'North'}
 loadProfile = {
-    0: [400, 600, 800, 550],
+    0: [400, 600, 760, 550],
     1: [50, 60, 70, 40]
 }
 transLimit = {1: 200, 2: 5000}
@@ -80,9 +86,10 @@ dispatchRes = {}
 revenue = {}
 profit = {}
 # renewable tax credit
-renewCredit = 50
+renewCredit = 30
 
 def showMarketInfo(roles):
+    global period
     with use_scope('market', clear=True):
         put_text('Generator Information:')
         tableHeader = ['Role ID', 'Fuel Type', 'Location', 'Player ID', 'Nameplate Capacity (MW)']
@@ -109,24 +116,32 @@ def showMarketInfo(roles):
         put_table(tableContent, header=tableHeader)
         put_text('Renewable Generation Forecast:')
         renewDf = pd.DataFrame()
-        period = [1, 2, 3, 4]
-        renewDf['Period'] = period
+        periodArray = [1, 2, 3, 4]
+        renewDf['Period'] = periodArray
         renewDf['Wind'] = [windTotal * w for w in windProfile]
         renewDf['Solar'] = [solarTotal * s for s in solarProfile]
         fig1 = px.bar(renewDf, x='Period', y=['Wind', 'Solar'])
         fig1.update_yaxes(range=[0, 1000])
         fig1.update_layout(yaxis_title='Generation (MW)')
+        fig1["data"][0]["marker"]["opacity"] = [1 if c == period else 0.5 for c in fig1["data"][0]["x"]]
+        fig1["data"][1]["marker"]["opacity"] = [1 if c == period else 0.5 for c in fig1["data"][1]["x"]]
+        fig1["data"][0]["marker"]["color"] = [plotly.colors.DEFAULT_PLOTLY_COLORS[0] for c in fig1["data"][0]["x"]]
+        fig1["data"][1]["marker"]["color"] = ['orange' for c in fig1["data"][1]["x"]]
         html = fig1.to_html(include_plotlyjs="require", full_html=False)
         put_html(html)
+
         put_text('Load Forecast:')
         loadDf = pd.DataFrame()
-        period = [1, 2, 3, 4]
-        loadDf['Period'] = period
+        loadDf['Period'] = periodArray
         loadDf['South'] = loadProfile[0]
         loadDf['North'] = loadProfile[1]
         fig2 = px.bar(loadDf, x='Period', y=['South', 'North'])
         fig2.update_yaxes(range=[0, 1000])
         fig2.update_layout(yaxis_title='Load (MW)')
+        fig2["data"][0]["marker"]["opacity"] = [1 if c == period else 0.5 for c in fig2["data"][0]["x"]]
+        fig2["data"][1]["marker"]["opacity"] = [1 if c == period else 0.5 for c in fig2["data"][1]["x"]]
+        fig2["data"][0]["marker"]["color"] = [plotly.colors.DEFAULT_PLOTLY_COLORS[4] for c in fig1["data"][0]["x"]]
+        fig2["data"][1]["marker"]["color"] = [plotly.colors.DEFAULT_PLOTLY_COLORS[3] for c in fig1["data"][1]["x"]]
         html = fig2.to_html(include_plotlyjs="require", full_html=False)
         put_html(html)
 
@@ -159,7 +174,7 @@ def showBids(period):
 
 def showDispatch(period, roles):
     put_text(f'Locational Marginal Price (Market Clearing Price, $/MWh):\n South: {clearingPrice[period - 1][0]}, North: {clearingPrice[period - 1][1]}')
-    tableHeader = ['Role', 'Fuel', 'Location', 'Player', 'Bid Capacity (MW)', 'Bid Price ($/MW)', 'Dispatch Result (MW)', 'Accum. Revenue ($)', 'Accum. Profit ($)']
+    tableHeader = ['Role', 'Fuel', 'Location', 'Player', 'Bid Capacity (MW)', 'Bid Price ($/MW)', 'Dispatch Result (MW)', 'Accum. Revenue ($)', 'Accum. Profit ($)', 'Average Profit ($/MW)']
     tableContent = []
     for i in range(1, 7):
         if i in roleID_gameID.keys():
@@ -168,9 +183,17 @@ def showDispatch(period, roles):
             gameID = 'None'
         roleID = i
         role = roles[str(roleID)]
-        row = [str(roleID), role['Fuel'], locIdx_name[role['Location']], gameID, str(sum([bid.amount for bid in bids_period[period] if bid.id == roleID])), str(sum([bid.price for bid in bids_period[period] if bid.id == roleID])), str(dispatchRes[i][period - 1]), str(revenue[i]), str(profit[i])]
+        row = [str(roleID), role['Fuel'], locIdx_name[role['Location']], gameID, str(sum([bid.amount for bid in bids_period[period] if bid.id == roleID])), str(sum([bid.price for bid in bids_period[period] if bid.id == roleID])), str(dispatchRes[i][period - 1]), str(revenue[i][period - 1]), str(profit[i][period - 1])]
+        if sum(dispatchRes[i][:period]) == 0:
+            row.append('0')
+        else:
+            row.append(str(int(profit[i][period - 1] / sum(dispatchRes[i][:period]))))
         tableContent.append(row)
     put_table(tableContent, header=tableHeader)
+    if not test:
+        if not os.path.exists(f'./gameHistory/{portID}_{round}_{period}.pkl'):
+            with open(f'./gameHistory/{portID}_{round}_{period}.pkl', 'wb') as f:
+                pickle.dump(tableContent, f)
 
 def showMarketRes(roles):
     with use_scope('market', clear=True):
@@ -182,7 +205,7 @@ def showBidForm(role, roleID):
     global periodBid_submitted
     clear('market')
     with use_scope('bid', clear=True):
-        if periodBid_submitted[roleID - 1]:
+        if periodBid_submitted[roleID - 1] or period > 4:
             put_text("You have submitted the bid for current period, please wait for the market clearing results.")
         else:
             '''
@@ -198,7 +221,15 @@ def showBidForm(role, roleID):
                 capacity = role['Capacity (MW)']
             # bid made in each period by each generator
             bidPrice = input(f'Period {period} Bid Price ($/MW) for {capacity} MW capacity', type=NUMBER, placeholder='0')
-            bids_period[period].append(bid(capacity, bidPrice, role['Location'], roleID))
+            bidExist = False
+            if period > 4:
+                bidExist = True
+            else:
+                for b in bids_period[period]:
+                    if b.id == roleID:
+                        bidExist = True
+            if not bidExist:
+                bids_period[period].append(bid(capacity, bidPrice, role['Location'], roleID))
             periodBid_submitted[roleID - 1] = True
             '''
             bid = input_group("Make Generation Bids (Segment1+Segmeng2=capacity)",[
@@ -217,12 +248,12 @@ def clearMarket(roles):
             role = roles[str(i)]
             if role['Fuel'] in ['wind', 'solar']:
                 bidGen = renewBidLimit[str(i)]
-                bidPrice = -40
+                bidPrice = -renewCredit + 10
                 if role['Fuel'] == 'solar':
                     print(bidGen)
             else:
                 bidGen = role['Capacity (MW)']
-                bidPrice = role['Generation Cost ($/MWh)']
+                bidPrice = role['Generation Cost ($/MWh)'] + random.randint(0, int(0.2 * role['Generation Cost ($/MWh)']))
             bids_period[period].append(bid(bidGen, bidPrice, role['Location'], i))
     genSol, lmp = gridDispatch(bids_period[period], [loadProfile[0][period - 1], loadProfile[1][period - 1]], transLimit[round])
     for gen in genSol:
@@ -233,14 +264,20 @@ def clearMarket(roles):
         role = roles[str(i)]
         lmp = clearingPrice[period - 1][role['Location']]
         if role['Fuel'] in ['wind', 'solar']:
-            revenue[i] += (lmp + renewCredit) * dispatchRes[i][period - 1]
-            profit[i] += (lmp + renewCredit - role['Generation Cost ($/MWh)']) * dispatchRes[i][period - 1]
+            revenue[i][period - 1] += (lmp + renewCredit) * dispatchRes[i][period - 1]
+            profit[i][period - 1] += (lmp + renewCredit - role['Generation Cost ($/MWh)']) * dispatchRes[i][period - 1]
+            if period >= 2:
+                revenue[i][period - 1] += revenue[i][period - 2]
+                profit[i][period - 1] += profit[i][period - 2]
         else:
             if role['Fuel'] == 'coal':
                 if dispatchRes[i][period - 1] == 0:
-                    profit[i] -= role['Not-dispatched Penalty (per period)']
-            revenue[i] += lmp * dispatchRes[i][period - 1]
-            profit[i] += (lmp - role['Generation Cost ($/MWh)']) * dispatchRes[i][period - 1]
+                    profit[i][period - 1] -= role['Not-dispatched Penalty (per period)']
+            revenue[i][period - 1] += lmp * dispatchRes[i][period - 1]
+            profit[i][period - 1] += (lmp - role['Generation Cost ($/MWh)']) * dispatchRes[i][period - 1]
+            if period >= 2:
+                revenue[i][period - 1] += revenue[i][period - 2]
+                profit[i][period - 1] += profit[i][period - 2]
 
             
 
@@ -294,8 +331,8 @@ def control_GM(choice, roles):
                 elif role['Fuel'] == 'solar':
                     renewBidLimit[str(i)] = int(role['Nameplate Capacity (Maximum possible generation MW)'] * solarProfile[period - 1] * np.random.normal(1, 0.1))
                 dispatchRes[i] = []
-                revenue[i] = 0
-                profit[i] = 0
+                revenue[i] = [0, 0, 0, 0]
+                profit[i] = [0, 0, 0, 0]
             round = 2
 
 def checkID(id):
@@ -312,12 +349,13 @@ def makeRoleCard(role):
                     put_text(f'{attrib}: {locIdx_name[role[attrib]]}')
                 else:
                     put_text(f'{attrib}: {role[attrib]}')
+        img = open('./map.png', 'rb').read()  
+        put_image(img, width='200px')
 
 def main():
-    global roleID_gameID, dispatchRes, period
+    global roleID_gameID, dispatchRes, period, revenue, profit
     roles = json.load(open('./generators.json'))
     id = input('Please input your game ID', type=NUMBER, required=True, validate=checkID)
-
     # game master interface
     if id == 1000:
         if round == 1:
@@ -330,10 +368,10 @@ def main():
                 elif role['Fuel'] == 'solar':
                     renewBidLimit[str(i)] = int(role['Nameplate Capacity (Maximum possible generation MW)'] * solarProfile[period - 1] * np.random.normal(1, 0.1))
                 dispatchRes[i] = []
-                revenue[i] = 0
-                profit[i] = 0
+                revenue[i] = [0, 0, 0, 0]
+                profit[i] = [0, 0, 0, 0]
                 period = 1
-        put_text(roleDescription[2])
+        put_text(roleDescription[3])
         put_buttons(['View Market Information', 'Clear Market', 'Move to Next Round'], onclick=partial(control_GM, roles=roles))
     # player interface
     else:
@@ -354,7 +392,13 @@ def main():
                 period_copy = period
                 with use_scope('info', clear=True):
                     put_text(f'Round: {round}, Market Period: {period}')
-                    put_text(f'Accumulated Revenue: {revenue[roleID]}, Accumulated Profit: {profit[roleID]}')
+                    print(revenue[roleID][period - 1], profit[roleID][period - 1])
+                    put_text(f'Accumulated Revenue ($): {revenue[roleID][period - 2] if period >= 2 else 0}, Accumulated Profit ($): {profit[roleID][period - 2] if period >= 2 else 0}')
+                    if period >= 2:
+                        avgProfit = int(profit[roleID][period - 2] / sum(dispatchRes[roleID][:period-1]) if sum(dispatchRes[roleID][:period-1]) != 0 else 0)
+                    else:
+                        avgProfit = 0
+                    put_text(f'Average Profit ($/MW): {avgProfit}')
                     if role['Fuel'] in ['wind', 'solar']:
                         capacity = renewBidLimit[str(roleID)]
                         put_text(f'Generation Limit in this period: {capacity} MW')
@@ -367,4 +411,6 @@ def main():
 
 
 if __name__ == '__main__':
-    start_server(main, debug=True, port=8001, host="localhost")
+    portID = sys.argv[1]
+    test = True
+    start_server(main, port=portID, host='localhost')
